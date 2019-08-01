a2d6b0d692cf81d96c2d29e8d1f560eb87998a32
Sangwon Hyun <34639434+HyunSangWon@users.noreply.github.com>
2019-08-01 12:47:52 +0900

    Update crawling.js

--- crawling.js
+++ crawling.js
@@ -4,7 +4,7 @@ const log = console.log;
 
 const getHtml = async() => {
     try{
-        return await axios.get("https://sports.news.naver.com/wbaseball/index.nhn");
+        return await axios.get("https://sports.news.naver.com/wbaseball/index.nhn"); // 크롤링 하고자 하는 사이트
     }catch(error){
         console.error(error);
     }
@@ -14,7 +14,7 @@ getHtml()
     .then(html => {
         let ulList = [];
     const $ = cheerio.load(html.data);
-    const $bodyList = $("div.home_news ul.home_news_list").children("li");
+    const $bodyList = $("div.home_news ul.home_news_list").children("li"); // 크롤링 대상
 
     $bodyList.each(function(index, elem) {
       ulList[index] = {
